cff4d38b7b3b5657bdd3522b063309a2048a47f9
Owen-Cochell <owencochell@gmail.com>
2024-09-27 18:57:54 -0400

    Some changes for coverage

--- cbmc/hci_cmd_done/main_harness.c
+++ cbmc/hci_cmd_done/main_harness.c
@@ -2,6 +2,25 @@
 
 #include "zephyr/net/buf.h"
 
+void* atomic_ptr_clear(void* thing) {
+
+	// Allocate network buffer:
+
+	struct net_buf *buf = (struct net_buf *)malloc(sizeof(struct net_buf));
+
+	return buf;
+}
+
+int net_buf_id(const struct net_buf *buf) {
+	int res;
+
+	// ID will NOT exceed maximum size
+
+	__CPROVER_assume(res >= 0 && res < CONFIG_BT_BUF_CMD_TX_COUNT);
+
+	return res;
+}
+
 int harness() {
 
 	// Model input arguments
