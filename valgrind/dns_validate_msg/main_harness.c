28a4c2009f7d5f15c247cf2359eeddde0ed4cf34
Owen-Cochell <owencochell@gmail.com>
2024-10-26 14:18:06 -0400

    More changes to valgrind test

--- valgrind/dns_validate_msg/main_harness.c
+++ valgrind/dns_validate_msg/main_harness.c
@@ -1,54 +1,73 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(net_dns_resolve_client_sample, LOG_LEVEL_DBG);
+
+#include <zephyr/kernel.h>
 #include <zephyr/net/dns_resolve.h>
 #include <dns_pack.h>
 
 int dns_validate_msg(struct dns_resolve_context *ctx, struct dns_msg_t *dns_msg, uint16_t *dns_id,
 		     int *query_idx, struct net_buf *dns_cname, uint16_t *query_hash);
 
-int main() {
+int main(void) {
 
-    // Model inputs:
+	LOG_INF("In Main lbllalala");
 
-    // Query IDX MUST be negative:
+	// Model inputs:
 
-    int query_idx = -1;
+	// Query IDX MUST be negative:
 
-    // Model network buffer:
+	int query_idx = -1;
 
-    struct net_buf dns_cname;
+	// Model network buffer:
 
-    // Define the DNS resolve context:
+	struct net_buf dns_cname;
 
-    struct dns_resolve_context ctx;
+	// Define the DNS resolve context:
 
-    // Model DNS message:
+	struct dns_resolve_context ctx;
 
-    struct dns_msg_t dns_msg;
+	// Model DNS message:
 
-    // Set the message size:
+	struct dns_msg_t dns_msg;
 
-    dns_msg.msg_size = 10;
+	// Set the message size:
 
-    // Set the query offset to be big, past bounds:
+	const uint16_t msg_size = 12;
 
-    dns_msg.query_offset = 15;
+	dns_msg.msg_size = msg_size;
 
-    // Allocate the size amount:
+	// Set the query offset to be big, past bounds:
 
-    dns_msg.msg = malloc(dns_msg.msg_size);
+	dns_msg.query_offset = 0;
+	dns_msg.response_length = 0;
+	dns_msg.response_type = -21;
+	dns_msg.response_position = 65511;
+	dns_msg.answer_offset = 16;
 
-    // We want to skip the while loop, so the 6th value MUST be zero
+	// Define the DNS data:
 
-    dns_msg.msg[6] = 0;
+    // The 7th value MUST be zero to skip the while loop
 
-    // The below can be arbitrary:
+	// The 3rd value must be 0x8 to ensure we don't ignore the query
+	// Value is & with 0x80 and used in a ternary which returns 1 if the result is not zero.
+	// We want this & to be not zero so we can skip past a check that can fail
 
-    uint16_t dns_id = 0;
-    uint16_t query_hash = 0;
+	uint8_t msg[12] = {0, 0, 128, 1, 0, 1, 0, 0};
 
-    // Call function:
+	dns_msg.msg = msg;
 
-    int res = dns_validate_msg(&ctx, &dns_msg, &dns_id, &query_idx, &dns_cname, &query_hash);
+	// The below can be arbitrary:
+
+	uint16_t dns_id = 0;
+	uint16_t query_hash = 0;
+
+	// Call function:
+
+	int res = dns_validate_msg(&ctx, &dns_msg, &dns_id, &query_idx, &dns_cname, &query_hash);
+
+	LOG_INF("Done Main");
 }
