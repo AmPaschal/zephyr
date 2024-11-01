06c8d3588c081a678201a66ed8f5cb2bf2c794c2
Paschal Amusuo <amusuopaschal@gmail.com>
2024-11-01 15:25:44 -0400

    Fixed the harness. All necessary code covered and errors resolved.

--- cbmc/proofs/usb/bluetooth/buf_stub.c
+++ cbmc/proofs/usb/bluetooth/buf_stub.c
@@ -6,8 +6,13 @@
 struct net_buf *net_buf_alloc_fixed(struct net_buf_pool *pool,
 				    k_timeout_t timeout)
 {
-	struct net_buf *buf = malloc(sizeof(struct net_buf));
+
+    uint8_t net_buf_size;
+    __CPROVER_assume(net_buf_size > sizeof(struct net_buf));
+	struct net_buf *buf = malloc(net_buf_size);
     __CPROVER_assume(buf != NULL);
+
+    buf->user_data_size = net_buf_size - sizeof(struct net_buf);
 
     uint16_t size;
     __CPROVER_assume(size > 0 && size < 100);
@@ -18,19 +23,22 @@ struct net_buf *net_buf_alloc_fixed(struct net_buf_pool *pool,
 
     // if (union_decider)
 
-    buf->b.data = data;
-    buf->b.len = 0;
-    buf->b.size = size;
-    buf->b.__buf = data;
+    // buf->b.data = data;
+    // buf->b.len = 0;
+    // buf->b.size = size;
+    // buf->b.__buf = data;
 
 
-    // buf->__buf = data;
-    // buf->data = buf->__buf;
-    // buf->size = size;
-    // buf->len = 0;
+    buf->__buf = data;
+    buf->data = buf->__buf;
+    buf->size = size;
+    buf->len = 0;
+
     buf->ref   = 1U;
 	buf->flags = 0U;
 	buf->frags = NULL;
+
+    return buf;
 
 }
 
@@ -47,24 +55,32 @@ size_t net_buf_simple_headroom(const struct net_buf_simple *buf)
 	return buf->data - buf->__buf;
 }
 
-// size_t net_buf_simple_tailroom(const struct net_buf_simple *buf)
-// {
-// 	return buf->size - net_buf_simple_headroom(buf) - buf->len;
-// }
+size_t net_buf_simple_tailroom(const struct net_buf_simple *buf)
+{
+	return buf->size - net_buf_simple_headroom(buf) - buf->len;
+}
 
-// void *net_buf_simple_add(struct net_buf_simple *buf, size_t len)
-// {
-// 	uint8_t *tail = net_buf_simple_tail(buf);
+void *net_buf_simple_add(struct net_buf_simple *buf, size_t len)
+{
+	uint8_t *tail = net_buf_simple_tail(buf);
 
-// 	__CPROVER_assert(net_buf_simple_tailroom(buf) >= len, "Not enough tailroom in buf");
+	__CPROVER_assert(net_buf_simple_tailroom(buf) >= len, "Not enough tailroom in buf");
 
-// 	buf->len += len;
-// 	return tail;
-// }
+	buf->len += len;
+	return tail;
+}
 
-// void *net_buf_simple_add_mem(struct net_buf_simple *buf, const void *mem,
-// 			     size_t len)
-// {
+void *net_buf_simple_add_mem(struct net_buf_simple *buf, const void *mem,
+			     size_t len)
+{
 
-// 	return memcpy(net_buf_simple_add(buf, len), mem, len);
-// }
+	return memcpy(net_buf_simple_add(buf, len), mem, len);
+}
+
+void net_buf_unref(struct net_buf *buf) {
+
+    free(buf->__buf);
+    buf->data = NULL;
+    free(buf);
+
+}
