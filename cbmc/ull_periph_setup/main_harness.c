f5b40ca87c619657ccba3be63eb40738aaceed72
tlelievre26 <jackrabbit5567@gmail.com>
2024-11-09 21:08:37 -0500

    Step 6

--- cbmc/ull_periph_setup/main_harness.c
+++ cbmc/ull_periph_setup/main_harness.c
@@ -53,36 +53,50 @@
 
 #include "ll_sw/ull_llcp.h"
 
-// #include "ll_sw/pdu.h"
-// #include "util/memq.h"
-// #include "ll_sw/lll.h"
-// #include "ll_sw/lll_conn.h"
-// #include "ll_sw/lll_adv.h"
+
 
 int harness() {
 	// When you compile, your coverage will be very low. Replacing your current definition of 
 	// rx with this pointer and updating the pointer dereferencing below will fix the problem.
 	// 
-	// uint8_t size;
-	// __CPROVER_assume(size > sizeof(struct node_rx_pdu) + sizeof(struct pdu_adv));
-	// struct node_rx_pdu *rx = malloc(size);
-	// __CPROVER_assume(rx != NULL);
+	uint8_t size;
+	__CPROVER_assume(size > sizeof(struct node_rx_pdu) + sizeof(struct pdu_adv));
+	struct node_rx_pdu *rx = malloc(size);
+	__CPROVER_assume(rx != NULL);
+	struct node_rx_hdr rx_hdr;
+	memq_link_t link;
+	rx_hdr.link = &link;
+	rx -> hdr = rx_hdr;
+	struct node_rx_ftr rx_ftr;
+	struct node_rx_pdu *extra_rx_ftr = malloc(size);
+	__CPROVER_assume(extra_rx_ftr != NULL);
+	rx_ftr.extra = extra_rx_ftr;
+	rx -> rx_ftr = rx_ftr;
+
 	
-	struct node_rx_pdu rx;
+	// struct pdu_adv* pdu = malloc(sizeof(struct pdu_adv));
+	// memcpy(rx -> pdu, pdu, sizeof(struct pdu_adv));
+
+	// struct node_rx_pdu rx;
 	struct node_rx_ftr ftr;
+	struct node_rx_pdu *extra_rx = malloc(size);
+	__CPROVER_assume(extra_rx != NULL);
+	ftr.extra = extra_rx;
 	struct lll_conn lll;
 
 
 	struct lll_adv ftr_param;
 	struct lll_hdr ftr_hdr;
+	struct ll_adv_set adv;
+	ftr_hdr.parent = &adv;
 	ftr_param.hdr = ftr_hdr;
 	ftr.param = &(ftr_param);
 
 	struct lll_hdr lll_struct_hdr;
+	uint8_t lll_hdr_size;
+	struct ll_conn parent;
+	lll_struct_hdr.parent = &parent;
 	lll.hdr = lll_struct_hdr;
-
-	struct node_rx_hdr rx_hdr;
-	rx.hdr = rx_hdr;
 
 	// uint8_t len;
 	// __CPROVER_assume(len <= 100);
@@ -90,7 +104,7 @@ int harness() {
 	// __CPROVER_assume(pdu != NULL);
 	// rx.pdu = pdu;
 
-	ull_periph_setup(&rx, &ftr, &lll);
+	ull_periph_setup(rx, &ftr, &lll);
 }
 
 int main() {
