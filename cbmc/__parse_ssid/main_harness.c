d0660ef36155d48ceb7830dde01122f2c27679a1
tlelievre26 <jackrabbit5567@gmail.com>
2024-11-23 16:02:21 -0500

    Step 6

--- cbmc/__parse_ssid/main_harness.c
+++ cbmc/__parse_ssid/main_harness.c
@@ -1,17 +1,18 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 int harness() {
 
 	uint8_t str_size;
-	__CPROVER_assume(str_size <= 20);
+	__CPROVER_assume(str_size <= 20 && str_size > 0);
 	char* str = malloc(str_size);
-	_CPROVER_assume(str != NULL);
+	__CPROVER_assume(str != NULL);
 	__CPROVER_assume(str[str_size - 1] == '\0');
 	uint8_t ssid_size;
-	__CPROVER_assume(ssid_size <= 20);
-	char* ssid = malloc(20);
-	_CPROVER_assume(ssid != NULL);
+	__CPROVER_assume(ssid_size <= 20 && ssid_size > 0);
+	char* ssid = malloc(ssid_size);
+	__CPROVER_assume(ssid != NULL);
 	__CPROVER_assume(ssid[ssid_size - 1] == '\0');
 	__parse_ssid(str, ssid);
 }
