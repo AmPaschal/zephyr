e4e66c679d63d1dabd5fef4c3bd49e9ae9a81b90
Paschal Amusuo <amusuopaschal@gmail.com>
2024-11-24 01:01:31 -0500

    Resolved the remaining errors

--- cbmc/do_write_op_tlv/main_harness.c
+++ cbmc/do_write_op_tlv/main_harness.c
@@ -4,6 +4,15 @@
 #include <zephyr/net/lwm2m.h>
 
 
+int lwm2m_engine_validate_write_access(struct lwm2m_message *msg,
+                                       struct lwm2m_engine_obj_inst *obj_inst,
+                                       struct lwm2m_engine_obj_field **obj_field) {
+	uint8_t len;
+	__CPROVER_assume(len >= sizeof(struct lwm2m_engine_obj_field));
+	*obj_field = malloc(len);
+	__CPROVER_assume(*obj_field != NULL);
+
+	}
 
 int harness() {
 	struct lwm2m_message msg;
@@ -14,6 +23,17 @@ int harness() {
 	struct lwm2m_block_context *block_ctx = malloc(sizeof(struct lwm2m_block_context));
 	__CPROVER_assume(block_ctx != NULL);
 	msg.in.block_ctx = block_ctx;
+
+	struct coap_packet *cpkt = malloc(sizeof(struct coap_packet));
+	__CPROVER_assume(cpkt != NULL);
+	msg.in.in_cpkt = cpkt;
+
+	uint16_t data_len;
+	__CPROVER_assume(data_len < 100);
+	uint8_t *data = malloc(data_len);
+	__CPROVER_assume(data != NULL);
+	msg.in.in_cpkt->data = data;
+	msg.in.in_cpkt->max_len = data_len;
 
 	do_write_op_tlv(&msg);
 }
