9ae1edcd3d4dbe2df65326bf2a5681e0c108a594
Paschal Amusuo <amusuopaschal@gmail.com>
2024-11-24 03:23:40 -0500

    Resolved errors

--- cbmc/ieee802154_recv/main_harness.c
+++ cbmc/ieee802154_recv/main_harness.c
@@ -6,77 +6,97 @@
 #include "l2/ieee802154/ieee802154_frame.h"
 #include "l2/ieee802154/ieee802154_6lo.h"
 
-bool ieee802154_validate_frame(uint8_t *buf, uint8_t length, struct ieee802154_mpdu *mpdu) {
-	struct ieee802154_mpdu new_mpdu;
-	struct ieee802154_fcf_seq* fs = malloc(sizeof(struct ieee802154_fcf_seq));
-	__CPROVER_assume(fs != NULL);
-	struct ieee802154_address_field* dst_addr = malloc(sizeof(struct ieee802154_address_field));
-	__CPROVER_assume(dst_addr != NULL);
-	struct ieee802154_address_field* src_addr = malloc(sizeof(struct ieee802154_address_field));
-	__CPROVER_assume(src_addr != NULL);
-	uint8_t* payload = (uint8_t*) malloc(length); //I'm not at all confident this is what I'm supposed to be doing but I need this to be defined
-	__CPROVER_assume(payload != NULL);
-	// new_mpdu.payload = payload;
-	// new_mpdu.payload_length = length;
-	new_mpdu.mhr.fs = fs;
-	new_mpdu.mhr.dst_addr = dst_addr;
-	new_mpdu.mhr.src_addr = src_addr;
-	*mpdu = new_mpdu;
-	__CPROVER_assume(&(mpdu -> mhr) != NULL);
-	return validate_payload_and_mfr(mpdu, buf, payload, length);
-}
 
-size_t net_pkt_get_len(struct net_pkt *pkt) {
-	size_t rand;
-	return rand;
-}
 
 enum ieee802154_hw_caps get_capabilities(const struct device *dev) {
 	enum ieee802154_hw_caps rand;
 	return rand;
 }
 
+struct net_pkt *net_pkt_alloc_with_buffer_debug(struct net_if *iface,
+						size_t size,
+						sa_family_t family,
+						enum net_ip_protocol proto,
+						k_timeout_t timeout,
+						const char *caller,
+						int line) {
+
+							struct net_pkt *pkt = malloc(sizeof(struct net_pkt));
+							__CPROVER_assume(pkt != NULL);
+
+							struct net_buf* buf = malloc(sizeof(struct net_buf));
+							__CPROVER_assume(buf != NULL);
+
+							uint8_t size;
+							__CPROVER_assume(size > sizeof(struct ieee802154_fcf_seq) && size <= 100);
+							uint8_t* data = (uint8_t*) malloc(size);
+							__CPROVER_assume(data != NULL);
+							
+							buf -> data = data;
+							buf -> size = size;
+							__CPROVER_assume(buf->len <= size);
+							pkt->frags = buf;
+
+							return pkt;
+
+						}
+
+int tx(const struct device *dev, enum ieee802154_tx_mode mode,
+                    struct net_pkt *pkt, struct net_buf *frag) {
+						int ret;
+						return ret;
+					}
+
 int harness() {
 	struct net_if iface;
 	struct net_if_dev* if_dev = malloc(sizeof(struct net_if_dev));
 	__CPROVER_assume(if_dev != NULL);
+
 	struct device* dev = malloc(sizeof(struct device));
 	__CPROVER_assume(dev != NULL);
+
 	struct ieee802154_radio_api* radio_api = malloc(sizeof(struct ieee802154_radio_api));
 	__CPROVER_assume(radio_api != NULL);
-	radio_api -> get_capabilities = get_capabilities;
-	dev -> api = radio_api;
-	if_dev -> dev = dev;
+
+	uint8_t l2_size;
+	__CPROVER_assume(l2_size >= sizeof(struct ieee802154_context));
+	uint8_t* l2data = (uint8_t*) malloc(l2_size);
+	__CPROVER_assume(l2data != NULL);
+	if_dev->l2_data = l2data;
+
+	radio_api->get_capabilities = get_capabilities;
+	radio_api->tx = tx;
+	dev->api = radio_api;
+	if_dev->dev = dev;
 	iface.if_dev = if_dev;
 
-	struct net_pkt pkt;
-	struct net_linkaddr src;
-	struct net_linkaddr dst;
-	pkt.lladdr_src = src;
-	pkt.lladdr_dst = dst;
-	__CPROVER_assume(&pkt != NULL);
-	__CPROVER_assume(&(pkt.lladdr_src) != NULL);
-	__CPROVER_assume(&(pkt.lladdr_dst) != NULL);
+	struct net_pkt *pkt = malloc(sizeof(struct net_pkt));
+	__CPROVER_assume(pkt != NULL);
+
+	uint8_t len;
+	__CPROVER_assume(len < 100);
+	uint8_t* srcaddr = (uint8_t*) malloc(len);
+	__CPROVER_assume(srcaddr != NULL);
+	pkt->lladdr_src.addr = srcaddr;
+	pkt->lladdr_src.len = len;
+	
+	
 	struct net_buf* buf = malloc(sizeof(struct net_buf));
 	__CPROVER_assume(buf != NULL);
 	uint8_t size;
-	__CPROVER_assume(size <= 100);
+	__CPROVER_assume(size >= (sizeof(struct ieee802154_fcf_seq) + IEEE802154_EXT_ADDR_LENGTH + IEEE802154_EXT_ADDR_LENGTH + sizeof(struct ieee802154_aux_security_hdr)) && size <= IEEE802154_MTU);
 	uint8_t* data = (uint8_t*) malloc(size);
 	__CPROVER_assume(data != NULL);
 	buf -> data = data;
-	buf -> len = size;
-	// uint8_t fs_size;
-	// __CPROVER_assume(fs_size > sizeof(struct ieee802154_fcf_seq));
-	// struct ieee802154_fcf_seq* fs = (struct ieee802154_fcf_seq*) malloc(fs_size);
-	// __CPROVER_assume(fs != NULL);
-	// __CPROVER_assume(&(fs -> fc) != NULL);
+	buf -> size = size;
+	buf->frags = NULL;
 	
-	// buf -> data = fs;
-	// buf -> len = fs_size;
-	// buf -> frags = NULL;
-	pkt.frags = buf;
+	__CPROVER_assume(buf->len <= size);
+	
+	
+	pkt->frags = buf;
 
-	ieee802154_recv(&iface, &pkt);
+	ieee802154_recv(&iface, pkt);
 }
 
 int main() {
