97e60d76510ad0c337941b3e910cb50ec9607d1f
Paschal Amusuo <amusuopaschal@gmail.com>
2024-12-03 17:18:40 -0500

    Resolved all the errors, added a config variable to increase coverage and linked a new file to cover the vulnerable line.

--- cbmc/ull_periph_setup/main_harness.c
+++ cbmc/ull_periph_setup/main_harness.c
@@ -63,26 +63,46 @@ int harness() {
 	// When you compile, your coverage will be very low. Replacing your current definition of 
 	// rx with this pointer and updating the pointer dereferencing below will fix the problem.
 	// 
-	// uint8_t size;
-	// __CPROVER_assume(size > sizeof(struct node_rx_pdu) + sizeof(struct pdu_adv));
-	// struct node_rx_pdu *rx = malloc(size);
-	// __CPROVER_assume(rx != NULL);
+	uint8_t size;
+	__CPROVER_assume(size > sizeof(struct node_rx_pdu) + sizeof(struct pdu_adv));
+	struct node_rx_pdu *rx = malloc(size);
+	__CPROVER_assume(rx != NULL);
+
+	struct node_rx_pdu *rx_ftr_extra = malloc(sizeof(struct node_rx_pdu));
+	__CPROVER_assume(rx_ftr_extra != NULL);
+
+	rx_ftr_extra->hdr.link = malloc(sizeof(memq_link_t));
+	__CPROVER_assume(rx_ftr_extra->hdr.link != NULL);
 	
-	struct node_rx_pdu rx;
+	rx->rx_ftr.extra = rx_ftr_extra;
+
+	// struct node_rx_pdu rx;
 	struct node_rx_ftr ftr;
 	struct lll_conn lll;
 
 
 	struct lll_adv ftr_param;
 	struct lll_hdr ftr_hdr;
+	ftr_hdr.parent = malloc(sizeof(struct ll_adv_set));
+	__CPROVER_assume(ftr_hdr.parent != NULL);
 	ftr_param.hdr = ftr_hdr;
 	ftr.param = &(ftr_param);
 
+	uint8_t ftr_extra_size;
+	__CPROVER_assume(ftr_extra_size > sizeof(struct node_rx_pdu));
+	struct node_rx_pdu *ftr_extra = malloc(ftr_extra_size);
+	__CPROVER_assume(ftr_extra != NULL);
+	// ftr_extra->pdu = malloc(sizeof(struct node_rx_cs));
+	// __CPROVER_assume(ftr_extra->pdu != NULL);
+	ftr.extra = ftr_extra;
+
 	struct lll_hdr lll_struct_hdr;
+	lll_struct_hdr.parent = malloc(sizeof(struct ll_conn));
+	__CPROVER_assume(lll_struct_hdr.parent != NULL);
 	lll.hdr = lll_struct_hdr;
 
 	struct node_rx_hdr rx_hdr;
-	rx.hdr = rx_hdr;
+	rx->hdr = rx_hdr;
 
 	// uint8_t len;
 	// __CPROVER_assume(len <= 100);
@@ -90,7 +110,7 @@ int harness() {
 	// __CPROVER_assume(pdu != NULL);
 	// rx.pdu = pdu;
 
-	ull_periph_setup(&rx, &ftr, &lll);
+	ull_periph_setup(rx, &ftr, &lll);
 }
 
 int main() {
