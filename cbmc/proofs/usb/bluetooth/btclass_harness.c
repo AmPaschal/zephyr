6260fefe22cbb0ac3a3d35a2b81a6395fc7a5491
Paschal Amusuo <amusuopaschal@gmail.com>
2025-02-03 19:53:31 -0500

    Modified harness to call the correct function

--- cbmc/proofs/usb/bluetooth/btclass_harness.c
+++ cbmc/proofs/usb/bluetooth/btclass_harness.c
@@ -3,6 +3,8 @@
  * @author Taylor Le Lievre (tlelievr@purdue.edu)
  * @brief Harness for bluetooth_class_handler
  * @version 0.1
+ * 
+ *  
  * @date 2024-09-24
  * 
  * @copyright Copyright (c) 2024
@@ -16,16 +18,22 @@
 #include "zephyr/net/buf.h"
 #include "zephyr/usb/usb_ch9.h"
 
+struct net_buf *bt_buf_get_tx(enum bt_buf_type type, k_timeout_t timeout,
+			      const void *data, size_t size);
+
 void harness() {
 
     struct usb_setup_packet setup;
 
     uint32_t len;
-    __CPROVER_assume(len > 0 && len < 100);
+    __CPROVER_assume(len > 0);
     
     uint8_t *data = malloc(len);
     __CPROVER_assume(data != NULL);
 
-    bluetooth_class_handler(&setup, &len, &data);
+    enum bt_buf_type type;
+    k_timeout_t timeout;
+
+    bt_buf_get_tx(type, timeout, data, len);
 
 }
--- subsys/bluetooth/host/hci_raw.c
+++ subsys/bluetooth/host/hci_raw.c
@@ -176,10 +176,10 @@ struct net_buf *bt_buf_get_tx(enum bt_buf_type type, k_timeout_t timeout,
 
 	if (data && size) {
 		// Recreating CVE-2021-3835
-		// if (net_buf_tailroom(buf) < size) {
-		// 	net_buf_unref(buf);
-		// 	return NULL;
-		// }
+		if (net_buf_tailroom(buf) < size) {
+			net_buf_unref(buf);
+			return NULL;
+		}
 
 		net_buf_add_mem(buf, data, size);
 	}
