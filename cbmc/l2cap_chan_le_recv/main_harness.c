8d6602608a91ab96b81b90fd5aea19831d8a218f
Paschal Amusuo <amusuopaschal@gmail.com>
2025-03-04 16:10:06 -0500

    Completed harness

--- cbmc/l2cap_chan_le_recv/main_harness.c
+++ cbmc/l2cap_chan_le_recv/main_harness.c
@@ -15,55 +15,52 @@ void l2cap_chan_send_credits(struct bt_l2cap_le_chan *chan,
 	return;
 }
 
-void l2cap_chan_le_recv_seg(struct bt_l2cap_le_chan *chan,
-				   struct net_buf *buf) {
-					return;
-				   }
-
-bool test_and_dec(atomic_t *target) {
-	bool random;
-	return random;
-}
-
 static int recv(struct bt_l2cap_chan *chan, struct net_buf *buf) {
 	int random;
-	__CPROVER_assume(random <= 0);
 	return random;
 }
 
 static struct net_buf* alloc_buf(struct bt_l2cap_chan *chan) {
-	struct net_buf* buf = malloc(sizeof(struct net_buf));
+	uint8_t sdubuflen;
+	__CPROVER_assume(sdubuflen > sizeof(struct net_buf) + sizeof(uint16_t));
+	struct net_buf* buf = malloc(sdubuflen);
 	return buf;
 }
 
 
 int harness() {
-	struct bt_l2cap_le_chan chan;
-	struct net_buf buf;
+	struct bt_l2cap_le_chan *chan = malloc(sizeof(struct bt_l2cap_le_chan));
+	struct net_buf *buf = malloc(sizeof(struct net_buf));
+
+	__CPROVER_assume(chan != NULL);
+	__CPROVER_assume(buf != NULL);
 
 	uint16_t buf_len;
-	__CPROVER_assume(buf_len <= 100);
 	uint8_t* buf_field = malloc(buf_len);
-	buf.len = buf_len;
-	buf.data = buf_field;
+	buf->len = buf_len;
+	buf->data = buf_field;
 
-	struct bt_l2cap_chan_ops ops;
-	ops.recv = recv;
+	struct bt_l2cap_chan_ops *ops = malloc(sizeof(struct bt_l2cap_chan_ops));
+	__CPROVER_assume(ops != NULL);
+	ops->recv = recv;
 
 	bool includeAllocBuf;
 	if(includeAllocBuf) {
 		//If this function is defined the target function returns early
 		//So we need to have it be NULL sometimes to get 100% coverage
-		ops.alloc_buf = alloc_buf;
+		ops->alloc_buf = alloc_buf;
 	}
 	else {
-		ops.alloc_buf = NULL;
+		ops->alloc_buf = NULL;
 	}
-	struct bt_l2cap_chan chan2;
-	chan2.ops = &ops;
-	chan.chan = chan2;
+	chan->chan.ops = ops;
 
-	l2cap_chan_le_recv(&chan, &buf);
+	uint8_t sdubuflen;
+	__CPROVER_assume(sdubuflen > sizeof(struct net_buf) + sizeof(uint16_t));
+	struct net_buf *sdubuf = malloc(sdubuflen);
+	chan->_sdu = sdubuf;
+
+	l2cap_chan_le_recv(chan, buf);
 }
 
 int main() {
