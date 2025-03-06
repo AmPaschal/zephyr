bdda8dd5d36d019ee176e9db96a8debd8becf4f1
Paschal Amusuo <amusuopaschal@gmail.com>
2025-03-06 17:26:01 -0500

    Final changes

--- cbmc/__parse_ssid/main_harness.c
+++ cbmc/__parse_ssid/main_harness.c
@@ -9,12 +9,12 @@ int harness() {
 	__CPROVER_assume(str_size > 0);
 	char* str = malloc(str_size);
 	__CPROVER_assume(str != NULL);
-	__CPROVER_assume(str[str_size - 1] == '\0');
+	str[str_size - 1] = '\0';
 	uint8_t ssid_size;
 	__CPROVER_assume(ssid_size >= WIFI_SSID_MAX_LEN);
 	char* ssid = malloc(ssid_size);
 	__CPROVER_assume(ssid != NULL);
-	__CPROVER_assume(ssid[ssid_size - 1] == '\0');
+	ssid[ssid_size - 1] = '\0';
 	__parse_ssid(str, ssid);
 }
 
